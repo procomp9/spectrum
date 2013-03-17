921f1786c614ba990f77769b651c6a30731ecb28
Erik Madrid <emadrid@aptus.io>
2013-03-17 16:40:33 -0500

    initial commit; layout; home; community

---
+++ config.rb
@@ -0,0 +1,16 @@
+require 'susy'
+
+activate :directory_indexes
+activate :livereload
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Use relative URLs
+  activate :relative_assets
+end
